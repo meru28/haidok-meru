f0f2bcc0bfc318415010c27cf2c59c12435b47b2
merurend <merurw@gmail.com>
2019-01-22 00:57:01 +0700

    rubah margin & hilangin border radius

--- src/assets/jss/rs-kit.jsx
+++ src/assets/jss/rs-kit.jsx
@@ -200,8 +200,8 @@ const mainRaised = {
     marginLeft: "10px",
     marginRight: "10px"
   },
-  margin: "-60px 30px 0px",
-  borderRadius: "6px",
+  margin: "-60px 0px 0px",
+  borderRadius: "0px",
   boxShadow:
     "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
 };
