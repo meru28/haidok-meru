6c829ceb78eaea77fa7c28425640df207d966747
merurend <merurw@gmail.com>
2019-01-22 01:07:05 +0700

    rubah ukuran container cardSignUp

--- src/assets/jss/rs-kit/views/signUpPageStyle.jsx
+++ src/assets/jss/rs-kit/views/signUpPageStyle.jsx
@@ -64,7 +64,7 @@ const signupPageStyle = theme => ({
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);",
     marginTop: "10px",
     marginBottom: "10px",
-    padding: "40px 0px"
+    padding: "30px 0px"
   },
   textCenter: {
     textAlign: "center"
